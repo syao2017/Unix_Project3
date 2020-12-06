90ab1ead95c7515087ac0989b7b71fe339af30c5
syao2017 <syao2017@vt.edu>
2020-12-05 22:50:31 -0500

    finished the main aspect of the project just need to add the IO handeling

--- integer.py
+++ integer.py
@@ -1,30 +1,59 @@
-x = input("Please input a file name: ")
-stack = []
-count = 0
-f = open(x, 'r')
 
-text = f.read()
-f.close()
-for x in text:
-    if x == '(':
-        stack.append(x)
-        count = count + 1
-    elif x == '{':
-    	stack.append(x)
-    	count = count + 1
-    elif x == '[':
-    	stack.append(x)
-    	count = count + 1
-    elif x == ')':
-        if stack[count - 1] == '(':
-            stack.pop()
-            count = count - 1
-    elif x == '}':
-        if stack[count - 1] == '{':
-            stack.pop()
-            count = count - 1
-    elif x == ']':
-        if stack[count - 1] == '[':
-            stack.pop()
-            count = count - 1
-print(stack)
+numOfFiles = input("plese input the number of files: ")
+numOfFiles = int(numOfFiles)
+
+while numOfFiles > 0:
+    fileName = input("Please input a file name: ")
+    stack = []
+    count = 0
+    found = " "
+    file = open(fileName, 'r')
+    text = file.read()
+    file.close()
+    for x in text:
+        if x == '(':
+            stack.append(x)
+            count = count + 1
+        elif x == '{':
+            stack.append(x)
+            count = count + 1
+        elif x == '[':
+            stack.append(x)
+            count = count + 1
+        elif x == ')':
+            if stack[count - 1] == '(':
+                stack.pop()
+                count = count - 1
+            else:
+                found = x
+                break
+        elif x == '}':
+            if stack[count - 1] == '{':
+                stack.pop()
+                count = count - 1
+            else:
+                found = x
+                break
+        elif x == ']':
+            if stack[count - 1] == '[':
+                stack.pop()
+                count = count - 1
+            else:
+                found = x
+                break
+    print("File Name \t Line Number \t Expected Charcter \t Found Character ")
+    if  count == 0:
+            print(fileName + " No error found")
+    else:
+            line = str(count)
+            expected = stack[count - 1]
+            if expected == "(":
+                expected = ")"
+            if expected == "{":
+                expected = "}"
+            if expected == "[":
+                expected = "]"
+            print(fileName + "\t\t" + line + "\t\t" + expected + "\t\t" + found)
+    numOfFiles = numOfFiles - 1
+
+	
